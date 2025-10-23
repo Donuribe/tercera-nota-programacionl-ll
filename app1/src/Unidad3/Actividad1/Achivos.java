94d53cfada7d5886d56a70a34874dde30211b47e
Donuribe <nicolasuribesanchez40@gmail.com>
2025-10-22 20:34:25 -0500

    .

--- app1/src/Unidad3/Actividad1/Achivos.java
+++
@@ -1,9 +0,0 @@
-package Unidad3.Actividad1;
-
-public class Achivos {
-    private String NombreArchivo;
-
-    public ManejoArchivos(String NombreArchivo) {
-        this.NombreArchivo = NombreArchivo;
-    }
-}
---
+++ src/Unidad3/Actividad1/Achivos.java
@@ -0,0 +1,13 @@
+package Unidad3.Actividad1;
+
+public class Achivos {
+    private String nombreArchivo;
+
+    public manejoArchivos(String nombreArchivo) {
+        this.nombreArchivo = nombreArchivo;
+    }
+
+    public void guardarLibro(list<Libro> libros) {
+        // Lógica para guardar el libro en un archivo
+    }
+}
